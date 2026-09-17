7679e3683914b9078a79cf7b4ee50e2aecd43254
ciemek <ciemek@yahoo.ie>
2026-09-17 09:59:35 +0200

    Pin bootstrap to 6c7fcf092b07 (clean hash fix)

--- Install-STRUCTURA-FACTOR.ps1
+++ Install-STRUCTURA-FACTOR.ps1
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'
 # --- Stale ---
 $CLIENT = "sawaryn"
 $LAUNCHER_REPO = "structura-factor/structura-factor-launchers"
-$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@24dfb9b55e0802734508c499c86cf2459dab5a9e/bootstrap.ps1"
+$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@6c7fcf092b074d60be0eddb16e263659efe231ee/bootstrap.ps1"
 $BASE_DIR = $InstallPath
 $KEYS_DIR = "$BASE_DIR\klucze"
 $MEDIA_DIR = "$BASE_DIR\media"
