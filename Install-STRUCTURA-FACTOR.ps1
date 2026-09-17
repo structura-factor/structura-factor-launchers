9ae09161ed594466107ddceabbacad04176327c0
ciemek <ciemek@yahoo.ie>
2026-09-17 09:10:58 +0200

    Pin bootstrap to b794ff9e4531 (setup.ps1 empty path fix)

--- Install-STRUCTURA-FACTOR.ps1
+++ Install-STRUCTURA-FACTOR.ps1
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'
 # --- Stale ---
 $CLIENT = "sawaryn"
 $LAUNCHER_REPO = "structura-factor/structura-factor-launchers"
-$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@718d48330576bb06cb1a3d8f644fe051c113087a/bootstrap.ps1"
+$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@b794ff9e45312a2bc582ba5d50479661ff08fe6f/bootstrap.ps1"
 $BASE_DIR = $InstallPath
 $KEYS_DIR = "$BASE_DIR\klucze"
 $MEDIA_DIR = "$BASE_DIR\media"
