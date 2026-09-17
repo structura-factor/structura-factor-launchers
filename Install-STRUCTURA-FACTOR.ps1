a1509e36ef05c6bb24d9d1cce45751fe86f4144a
ciemek <ciemek@yahoo.ie>
2026-09-17 11:54:29 +0200

    fix: update $BOOTSTRAP_URL pin to fe0934ea11b5 (VBox install fix)

--- Install-STRUCTURA-FACTOR.ps1
+++ Install-STRUCTURA-FACTOR.ps1
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'
 # --- Stale ---
 $CLIENT = "sawaryn"
 $LAUNCHER_REPO = "structura-factor/structura-factor-launchers"
-$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@010ea7e76a5caac26d5707e9b4ba7d05ab6c5197/bootstrap.ps1"
+$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@fe0934ea11b5c25b060077b2049fc4d994a14d67/bootstrap.ps1"
 $BASE_DIR = $InstallPath
 $KEYS_DIR = "$BASE_DIR\klucze"
 $MEDIA_DIR = "$BASE_DIR\media"
