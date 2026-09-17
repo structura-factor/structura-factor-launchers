699f43aca5631fee4bd1e4d6282abaaa060db4fa
ciemek <ciemek@yahoo.ie>
2026-09-17 14:39:13 +0200

    pin -> 35511c7d855a

--- Install-STRUCTURA-FACTOR.ps1
+++ Install-STRUCTURA-FACTOR.ps1
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'
 # --- Stale ---
 $CLIENT = "sawaryn"
 $LAUNCHER_REPO = "structura-factor/structura-factor-launchers"
-$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@13066f958462d131dbf4f27ba0f25b9e44bf05e2/bootstrap.ps1"
+$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@35511c7d855a13af1ccb24518b7bcdea98fcf2bc/bootstrap.ps1"
 $BASE_DIR = $InstallPath
 $KEYS_DIR = "$BASE_DIR\klucze"
 $MEDIA_DIR = "$BASE_DIR\media"
