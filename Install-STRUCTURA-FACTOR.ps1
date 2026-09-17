15c31362f96985fcc101349b159bb1024715aae9
ciemek <ciemek@yahoo.ie>
2026-09-17 10:35:36 +0200

    fix: update $BOOTSTRAP_URL pin to 010ea7e76a5c (was 6c7fcf092b07 with stale bootstrap.ps1)

--- Install-STRUCTURA-FACTOR.ps1
+++ Install-STRUCTURA-FACTOR.ps1
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'
 # --- Stale ---
 $CLIENT = "sawaryn"
 $LAUNCHER_REPO = "structura-factor/structura-factor-launchers"
-$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@6c7fcf092b074d60be0eddb16e263659efe231ee/bootstrap.ps1"
+$BOOTSTRAP_URL = "https://cdn.jsdelivr.net/gh/structura-factor/structura-factor-launchers@010ea7e76a5caac26d5707e9b4ba7d05ab6c5197/bootstrap.ps1"
 $BASE_DIR = $InstallPath
 $KEYS_DIR = "$BASE_DIR\klucze"
 $MEDIA_DIR = "$BASE_DIR\media"
