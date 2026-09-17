5db8906e86b302b843a6bd30eea15fc1dcf4c0c0
ciemek <ciemek@yahoo.ie>
2026-09-17 15:45:44 +0200

    pin -> 7dd61afcf1b8

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -20,7 +20,7 @@ param(
     [string]$MediaPath,
 
     [Parameter(Mandatory = $false)]
-    [string]$LauncherRepo = "structura-factor/structura-factor-launchers@2b0b071d9ce21c3b92b8f49385fe9e4cef414b2a",
+    [string]$LauncherRepo = "structura-factor/structura-factor-launchers@7dd61afcf1b8d0e2f31905fd9ab9bf3233f67536",
 
     [Parameter(Mandatory = $false)]
     [string]$ClientRepo = "structura-factor/structura-clients-sawaryn",
