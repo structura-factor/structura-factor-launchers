fd9192fd1ee7a0db68d26be7c2fe440fc017e1ec
ciemek <ciemek@yahoo.ie>
2026-09-17 07:23:48 +0200

    Add -InstallPath parameter (default C:\STRUCTURA, parameterized temp clone path)

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -11,6 +11,9 @@ param(
     [string]$Client,
 
     [Parameter(Mandatory = $false)]
+    [string]$InstallPath = "C:\STRUCTURA",
+
+    [Parameter(Mandatory = $false)]
     [string]$DeployKeyPath,
 
     [Parameter(Mandatory = $false)]
@@ -184,7 +187,7 @@ if ($MediaPath) { Write-StructuraLog "Media path: $MediaPath" }
 # Step 1: Fetch bootstrap.yaml from client repo via git (private repo)
 Write-StructuraLog "Fetching bootstrap.yaml from $ClientRepo (private, via git)..."
 
-$tempClone = "C:\STRUCTURA\temp\bootstrap-clone"
+$tempClone = "$InstallPath\temp\bootstrap-clone"
 $tempParent = Split-Path $tempClone -Parent
 if (-not (Test-Path $tempParent)) { New-Item -ItemType Directory -Path $tempParent -Force | Out-Null }
 if (Test-Path $tempClone) { Remove-Item $tempClone -Recurse -Force }
