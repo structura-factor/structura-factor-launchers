b0a854b4f8a9479184217c1ffef190023d401d52
ciemek <ciemek@users.noreply.github.com>
2026-09-22 13:15:52 +0200

    pin: bootstrap -> setup.ps1 @e936c0d

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -26,7 +26,7 @@ param(
     [switch]$SkipMediaVerify,
 
     [Parameter(Mandatory = $false)]
-    [string]$LauncherRepo = "structura-factor/structura-factor-launchers@80831246dc813ebb06bd1ba910d54a12c988423c",
+    [string]$LauncherRepo = "structura-factor/structura-factor-launchers@e936c0d04e4377eab8dfe590ec70e8bb6eefcf5d",
     # Jawny wybor launchera (nadpisuje 'launcher' z bootstrap.yaml).
     # Uzycie: .\bootstrap.ps1 -Client sawaryn -Launcher factor-vm-win11
     # Przydatne przy testach bez specyfikacji klienta.
