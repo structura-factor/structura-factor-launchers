6d7d32a26abd2e6b51cb2615b3dcbec724903aec
S <s@l>
2026-09-23 15:10:10 +0200

    Re-pin: bootstrap -> launchers@d0abc34 (poweroff zamiast reboot)

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -26,7 +26,7 @@ param(
     [switch]$SkipMediaVerify,
 
     [Parameter(Mandatory = $false)]
-    [string]$LauncherRepo = "structura-factor/structura-factor-launchers@5fda8e8e4f15821929b9b2ca475f3ad0860f6282",
+    [string]$LauncherRepo = "structura-factor/structura-factor-launchers@d0abc3429f4325d2548d478bc5f62642bf8312f0",
     # Jawny wybor launchera (nadpisuje 'launcher' z bootstrap.yaml).
     # Uzycie: .\bootstrap.ps1 -Client sawaryn -Launcher factor-vm-win11
     # Przydatne przy testach bez specyfikacji klienta.
